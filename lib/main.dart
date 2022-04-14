af525c42e9b559f62cd10de223f592729c56ecac
suliman <sulimanawadstudy@gmail.com>
2022-04-14 18:36:43 +0300

    add the firebase_options.dart file to the lib folder by flutterfire_cli and coordinating.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'application/app.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyCqhqdma4I2IlGT30Phi9pD8F_zhyQcm9Y",
-      appId: "1:234878303253:web:16ac3b7c4f7b347540221c",
-      messagingSenderId: "234878303253",
-      projectId: "prism-7ca75",
-    ),
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Application());
 }
