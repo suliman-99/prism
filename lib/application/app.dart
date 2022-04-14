bf3e021c0ee26d45906dc8863bcc9ba28508ee61
suliman <sulimanawadstudy@gmail.com>
2022-04-14 03:05:04 +0300

    Connect to firestore and firebase storage with testing and deploying.

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -1,16 +1,50 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Application extends StatelessWidget {
-  Application({Key? key}) : super(key: key);
-
-  final Future<FirebaseApp> initialization = Firebase.initializeApp();
+  const Application({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Center(
-        child: Text('Deployed By Abo Iscandar'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Column(
+        children: [
+          const Text('Deployed By Abo Iscander'),
+          const SizedBox(
+            height: 20,
+          ),
+          StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('Categories').snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return const Text('ERROR .');
+              }
+              if (snapshot.connectionState == ConnectionState.active) {
+                return SizedBox(
+                  height: 400,
+                  child: ListView(
+                    children: snapshot.data!.docs.map((dynamic e) {
+                      String name = e.id;
+                      String len = e.data()['products'].length.toString();
+                      return Column(
+                        children: [
+                          Text("The Number of the Product in $name is $len ."),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                        ],
+                      );
+                    }).toList(),
+                  ),
+                );
+              }
+              return const CircularProgressIndicator();
+            },
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'application/app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(Application());
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCqhqdma4I2IlGT30Phi9pD8F_zhyQcm9Y",
+      appId: "1:234878303253:web:16ac3b7c4f7b347540221c",
+      messagingSenderId: "234878303253",
+      projectId: "prism-7ca75",
+    ),
+  );
+  runApp(const Application());
 }
